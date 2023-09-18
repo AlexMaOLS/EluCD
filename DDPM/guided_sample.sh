0b9e5e061a0545b5c88b7a7173907096feea7663
AlexMaOLS <jmabh@connect.ust.hk>
2023-09-18 17:10:18 +0800

    add

--- DDPM/guided_sample.sh
+++ DDPM/guided_sample.sh
@@ -7,7 +7,7 @@ SAMPLE_FLAGS="--log_dir ./save_sample_images/resnet101_c1.5_soft3_jt1.0+mt0.5_mu
 
 MODEL_FLAGS="--attention_resolutions 32,16,8 --class_cond True --image_size 128 --learn_sigma True --num_channels 256 --num_heads 4 --num_res_blocks 2 --resblock_updown True --use_fp16 True --use_scale_shift_norm True"
 
-CLASSIFIER_FLAGS="--image_size 128 --classifier_attention_resolutions 32,16,8 --classifier_depth 2 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True --classifier_scale 1.0 --softplus_beta 3.0 --joint_temperature 1.0 --margin_temperature_discount 0.5 --good_class_factor 1.5 --classifier_use_fp16 True"
+CLASSIFIER_FLAGS="--image_size 128 --classifier_attention_resolutions 32,16,8 --classifier_depth 2 --classifier_width 128 --classifier_pool attention --classifier_resblock_updown True --classifier_use_scale_shift_norm True --classifier_scale 1.0 --softplus_beta 3.0 --joint_temperature 1.0 --margin_temperature_discount 0.5 --classifier_use_fp16 True"
 
 
 mpiexec -n 8 python scripts/classifier_sample.py \  
--- pytorch-fid-master/src/evaluation_image.sh
+++ pytorch-fid-master/src/evaluation_image.sh
@@ -2,7 +2,7 @@ pkill python
 
 #!/bin/bash
 
-python -m pytorch_fid --num-samples 50000 '../../pretrained_models/VIRTUAL_imagenet128_labeled.npz' '../../save_sample_images/resnet101_c1.5_soft3_jt1.0+mt0.5_mu0.3_50000_Imagefolder'
+python -m pytorch_fid --num-samples 50000 'path of the reference data' 'path of the generated image folder'
 
 
  
